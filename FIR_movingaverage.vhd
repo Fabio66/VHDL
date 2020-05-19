fc3fe57665cae63da80989994b5fbb8eda79d25f
Fabio66 <fabio.polimi77@gmail.com>
2020-05-19 15:21:27 +0200

    Signed versions corrected and STD_LOGIC_VECTOR versions added
    Some syntax errors corrected

--- FIR_movingaverage.vhd
+++ FIR_movingaverage.vhd
@@ -22,36 +22,43 @@ end FIR;
 
 architecture Behavioral of FIR is 
 
-    constant init_fir : signed(8*WORD_BYTES-1 downto 0) := (Others => '0');
-    type shiftreg is array (0 to 2**TO_EXTEND-1) of signed(8*WORD_BYTES-1 downto 0) := (others => init_fir);
+    --We need to declare two constant cause init_sum and init_fir are of different "dimensions" 
+    constant init_fir : signed(8*WORD_BYTES-1 downto 0)             := to_signed(INIT,Data_in'HIGH);
+    constant init_sum : signed(TO_EXTEND+8*WORD_BYTES-1 downto 0)   := to_signed((2**TO_EXTEND)*INIT,TO_EXTEND+Data_in'HIGH);
 
-    signal sum : signed(TO_EXTEND+8*WORD_BYTES-1 downto 0) := (others => '0');
+    --Type declaration
+    type shiftreg is array (0 to 2**TO_EXTEND-1) of signed(8*WORD_BYTES-1 downto 0);
+    --"Type" instatiation
+    signal shift_reg0 : shiftreg := (others => init_fir);
+
+    signal sum        : signed(TO_EXTEND+8*WORD_BYTES-1 downto 0)   := init_sum;
 
 begin
 
     process(clk)
-        --variable temp : unsigned(TO_EXTEND+8*WORD_BYTES-1 downto 0);
+
     begin
         if reset = '1' then
-            shiftreg <= (others => init_fir);
-            sum      <= (others => '0');
+            --(2**TO_EXTEND * init_fir ) = init_sum
+            shift_reg0  <= (others => init_fir);
+            sum         <= init_sum;
        
         elsif rising_edge(clk) then
             if CE = '1' then 
                 --If we take directly the input, we do not need to store the std_logic_vectotr referred as TO_WEIGHT-1
                 --shiftreg_exit <= shiftreg(TO_WEIGHT-1);
-                shiftreg(1 to 2**TO_EXTEND-1)   <= shiftreg(0 to 2**TO_EXTEND-2); 
-                shiftreg(0)                     <= Data_in;
+                shift_reg0(1 to 2**TO_EXTEND-1)         <= shift_reg0(0 to 2**TO_EXTEND-2); 
+                shift_reg0(0)                           <= Data_in;
                 
-                sum                             <= sum + Data_in - shiftreg(2**TO_EXTEND-1);
+                sum                                     <= sum + Data_in - shift_reg0(2**TO_EXTEND-1);
                 
-                Data_out                        <= sum(sum'HIGH downto TO_EXTEND);
+                Data_out                                <= sum(sum'HIGH downto TO_EXTEND);
            
-            else--that is, CE = '0' or even the other unused type of std_logic
+            else ----that is, CE = '0' or even the other unused type of std_logic
                
                 --Remain still and do not care about the input
-                shiftreg(0 to 2**TO_EXTEND-1) <= shiftreg(0 to 2**TO_EXTEND-1);
-                Data_out                        <= sum(sum'HIGH downto TO_EXTEND);
+                shift_reg0(0 to 2**TO_EXTEND-1)         <= shiftreg(0 to 2**TO_EXTEND-1);
+                Data_out                                <= sum(sum'HIGH downto TO_EXTEND);
             
             end if;
         end if;
